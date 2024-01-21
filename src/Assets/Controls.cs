5af25159736590477e2ba02fa433facb71dc06f7
SZunaidA <fakehotdog03@gmail.com>
2024-01-21 19:43:32 +1100

    idk

--- src/Assets/Controls.cs
+++ src/Assets/Controls.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 1.0F;
     void Start()
     {
     }
@@ -14,10 +15,18 @@ public class Controls : MonoBehaviour
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.A)) {
-            transform.position = transform.position + new Vector3(-1, 0, 0);
+            transform.position = transform.position + new Vector3(-speed, 0, 0);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.position = transform.position + new Vector3(1, 0, 0);
+            transform.position = transform.position + new Vector3(speed, 0, 0);
         }
+        if (Input.GetKeyDown(KeyCode.W) && canJump()) {
+            Debug.Log("pressing W");
+            transform.position = transform.position + new Vector3(0, 1F, 0);
+        }
+    }
+
+    private bool canJump() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 }
